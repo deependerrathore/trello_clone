21bc0085f6103dafc1119f02ccda6b7ac2be461b
Deepender Rathore <deepenrathore28@gmail.com>
2019-03-20 12:51:33 +1100

    add feathers-vuex to client"

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import feathersVuex from 'feathers-vuex'
+import feathersClient from './feathers-client'
+
+const {auth, FeathersVuex } = feathersVuex(feathersClient, { idField: '_id' })
+
 Vue.use(Vuex);
+Vue.use(FeathersVuex)
 
 export default new Vuex.Store({
-  state: {
+  plugins:[
+    auth({ userService: 'users' })
+  ]
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
 });
+
+//https://feathers-vuex.feathers-plus.com/auth-module.html
--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -6,8 +6,23 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
   
-    email: {type: String, unique: true, lowercase: true},
-    password: { type: String },
+    username: {
+      type: String, 
+      unique: true, 
+      lowercase: true
+    },
+    displayName: {
+      type: String, 
+      required: true
+    },
+    imageUrl: {
+      type: String, 
+      required: true
+    },
+    password: { 
+      type: String,
+      required: true
+    },
   
   
   }, {
